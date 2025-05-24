6b4ca227ffce149fa8322010fb9593829557b1f5
Sinha Milan <milans0415@gmail.com>
2025-05-24 11:56:30 +0530

    script.js

--- script.js
+++ script.js
@@ -5,21 +5,16 @@ function bigShow(name) {
 
     document.addEventListener('keydown', function(e) {
         if(e.key==='Escape') {
-            closeIt();
+            document.getElementById(name).style.display = "none";
+            document.getElementById(name).style.animationName = "";
         }
     })
-    document.getElementById(name).addEventListener('click', (e) => {
-        if (e.target === document.getElementById(name)) {
-            closeIt();
+    window.addEventListener('popstate', (event) => {
+        if (document.getElementById(name).style.display === 'flex') {
+            document.getElementById(name).style.display = 'none';
         }
     });
 }
-
-function closeIt() {
-    document.getElementById(name).style.display = "none";
-    document.getElementById(name).style.animationName = "";
-}
-
 
 function openLink(Id) {
     if(Id.id==='l') {
